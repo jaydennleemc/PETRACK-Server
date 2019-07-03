a15f211b69fb82e7641b070dcaf0811f6f16c441
jayyyden <jaydennleemc@gmail.com>
2019-07-03 17:14:18 +0800

    added update pet function" ; `

--- api/controllers/petController.js
+++ api/controllers/petController.js
@@ -81,6 +81,53 @@ exports.getPet = function (req, res) {
 
 exports.updatePet = function (req, res) {
    console.log('updatePet API was called');
+   const id = req.body.id;
+   const name = req.body.name;
+   const gender = req.body.gender;
+   const type = req.body.type;
+   const birthdate = req.body.birthdate;
+   const weight = req.body.weight;
+
+   if (req.headers.authorization != null) {
+      const token = req.headers.authorization.replace('Bearer ', '')
+      const result = JSON.parse(utils.verifyJWT(token));
+      mongoHelper.findOne(result.id).then(user => {
+         if (user != null) {
+            if (user.pets != null && user.pets.length > 0) {
+               var pet = {
+                  'id': id,
+                  'name': name,
+                  'gender': gender,
+                  'type': type,
+                  'birthdate': birthdate,
+                  'weight': weight,
+               };
+               pets = user.pets.filter(function(value) {
+                  if(value.id = id) {
+                     value = pet;
+                  }
+                  return value;
+               })
+               user.pets = pets
+            }
+            mongoHelper.updateDocument(user.id, user).then(result => {
+               if (result) {
+                  res.json({
+                     'code': 0,
+                     'message': 'Success'
+                  });
+
+               } else {
+                  res.json({
+                     'code': 1,
+                     'message': 'Success'
+                  });
+               }
+            })
+         }
+
+      })
+   }
 
    res.json({
       'code': 0,
